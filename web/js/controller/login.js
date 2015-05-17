ddb7e83e01480ee277094f18bb77f6ca1686eb59
zihanwang <zihanwangthestreet@gmail.com>
2015-05-17 12:48:06 -0400

    add login and loginctrl to project

--- web/js/controller/login.js
+++ web/js/controller/login.js
@@ -1,3 +1,9 @@
 /**
  * Created by zihanwang on 5/16/15.
  */
+angular.module('uiRouterSample.contacts.service', [])
+    .controller("loginCtrl", function($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+        console.log($scope);
+});
