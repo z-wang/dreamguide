240ff0201eaea1ae329239f54fa6f1cfea87f58c
zihanwang <zihanwangthestreet@gmail.com>
2015-05-17 21:04:35 -0400

    delete datamap and use highmap instead

--- web/js/controller/login.js
+++ web/js/controller/login.js
@@ -3,7 +3,10 @@
  */
 angular.module('dgApp.controllers', [])
     .controller("loginCtrl", function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
+        $(window).unbind("scroll");
+        $('.navbar').unbind('mouseenter mouseleave');
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $scope.firstName = "John";
+        $scope.lastName = "Doe";
         console.log($scope);
 });
