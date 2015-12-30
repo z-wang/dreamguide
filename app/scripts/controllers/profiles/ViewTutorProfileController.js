866cd1360e28670558648e2ca0034da1025e007e
codingmanny <codingmanny@gmail.com>
2015-12-30 08:21:18 -0800

    edit tutor info page

--- app/scripts/controllers/profiles/ViewTutorProfileController.js
+++ app/scripts/controllers/profiles/ViewTutorProfileController.js
@@ -22,6 +22,20 @@ define(['app'], function(app)
                     $scope.thisTutor.image = 'image/avatar/'+ tutorId+'.png';
                 };
 
+                $scope.image = {
+                    defaultImage : 'image/avatar/zz@test.com.png'
+                };
+
+                $scope.rating = {
+                    total : 5,
+                    current : 4,
+                    isReadOnly : true
+                };
+
+                $scope.changeStar = function(){
+                    console.log($scope.rating);
+                };
+
                 searchService.getByIdCallBack('users','accounts', tutorId, getTutorCallBack);
                 console.log($rootScope);
             }
