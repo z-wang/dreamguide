c7f311c3f5cdd323dd327726e9892b8dfe2c32ae
codingmanny <codingmanny@gmail.com>
2016-01-08 00:59:09 -0500

    edit eselection front end

--- app/scripts/controllers/tools/eselection/InputController.js
+++ app/scripts/controllers/tools/eselection/InputController.js
@@ -17,10 +17,11 @@ define([
                 $('.navbar').unbind('mouseenter mouseleave');
                 $(".navbar-fixed-top").addClass("top-nav-collapse");
 
-                $scope.tutor={
+                $scope.tutor = {
                     gradSchool : "",
                     gradMajor : ""
                 };
+
                 $scope.toRegister = function(){
                     $(window).scrollTop(0);
                     $location.path("/register");
@@ -30,26 +31,68 @@ define([
                 $scope.undergradSchools = ["北大清华", "C9",  "985", "211", "本专业强校(top 30)", "其他"];
                 $scope.applyDegrees = ["高中", "本科", "硕士", "博士"];
                 $scope.applyMajors = ["金融", "会计", "电子工程", "计算机"];
-                $scope.input = {};
+                //$scope.input = {};
+                $scope.input = {
+                    applyMajor : "金融",
+                    applyDegree : "硕士",
+                    internship : 0,
+                    fulltime : 0,
+                    patent : 0,
+                    publication : 0
+                };
+                var schoolValues = {
+                    "北大清华" : 1,
+                    "C9" : 2,
+                    "985" : 3,
+                    "211" : 4,
+                    "本专业强校(top 30)" : 5,
+                    "其他" : 6
+                };
 
 
                 $scope.submitApply = function(){
-                    $http.post('/tools/eselection/inputquery', {
-                        input: $scope.input
+                    var rawData = {
+                        undergradSchool : Number(schoolValues[$scope.input.undergradSchool]),
+                        toefl: Number($scope.input.toefl),
+                        gre: Number($scope.input.gre),
+                        gpa: Number($scope.input.gpa)
+                    };
+                    console.log(rawData);
+
+                    $http.post('/tools/eselection/getPredictions', {
+                        input: rawData
                     }).
                         success(function(data, status, headers, config) {
-                            console.log(data);
+                            $scope.results = data;
+                            console.log("prediction success");
                         }).
                         error(function(data, status, headers, config) {
                             console.log(data);
                         });
+                    //$http.post('/tools/eselection/inputquery', {
+                    //    input: $scope.input
+                    //}).
+                    //    success(function(data, status, headers, config) {
+                    //        console.log(data);
+                    //    }).
+                    //    error(function(data, status, headers, config) {
+                    //        console.log(data);
+                    //    });
 
                     //$location.path("/thankyou");
                 };
 
                 $scope.reset = function(){
                     console.log($scope.input);
-                    $scope.input = {};
+
+                    $scope.input = {
+                        applyMajor : "金融",
+                        applyDegree : "硕士",
+                        internship : 0,
+                        fulltime : 0,
+                        patent : 0,
+                        publication : 0
+                    };
                 };
             }]);
 });
