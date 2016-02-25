4a07d29cbf67c13612bc2e28a8e388896edb0152
codingmanny <codingmanny@gmail.com>
2016-02-25 01:34:44 -0500

    add FE

--- data/tools/eselections/getEselection.js
+++ data/tools/eselections/getEselection.js
@@ -3,12 +3,17 @@
  */
 var financeData = require('./finance/eselection');
 var financeRank = require('./finance/ranks');
-
+var FEData = require('./financial_engineering/eselection');
+var FERank = require('./financial_engineering/ranks');
 
 module.exports = {
     finance : {
         eselectionData : financeData,
         schoolRank : financeRank
+    },
+    FE : {
+        eselectionData : FEData,
+        schoolRank : FERank
     }
 };
 
